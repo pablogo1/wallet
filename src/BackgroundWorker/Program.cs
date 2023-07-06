17be7425ba5c95e9782ab716619b6bd096cb355e
Pablo Gonzalez <pablogr.gonzalez@gmail.com>
2023-07-06 11:56:06 -0600

    Squashed commit of the following:
    commit ad399e745246f3fe114cb28cc163b8ef33c9ab8f
    Author: Pablo Gonzalez <pablogr.gonzalez@gmail.com>
    Date:   Sat Jun 3 18:23:59 2023 -0600
    
        Configure deps
    
        Signed-off-by: Pablo Gonzalez <pablogr.gonzalez@gmail.com>
    
    commit 8260c0779975cc0f42885b43a699fa2f65071171
    Author: Pablo Gonzalez <pablogr.gonzalez@gmail.com>
    Date:   Sat Jun 3 17:33:18 2023 -0600
    
        Removed template examples.
    
    commit 0e4ca334d3e2abf390e9875256a7eb98a98e1649
    Author: Pablo Gonzalez <pablogr.gonzalez@gmail.com>
    Date:   Sat Jun 3 17:18:12 2023 -0600
    
        Clean example template code.
    
        Signed-off-by: Pablo Gonzalez <pablogr.gonzalez@gmail.com>
    
    commit b053bc26f0be9b15678d089adb9920db1d882a74
    Author: Pablo Gonzalez <pablogr.gonzalez@gmail.com>
    Date:   Sat Jun 3 16:41:34 2023 -0600
    
        Fixed Page styles
    
    commit be0e080b773260bfc4365987928c1a38c13f7955
    Author: Pablo Gonzalez <pablogr.gonzalez@gmail.com>
    Date:   Fri Jun 2 13:49:57 2023 -0600
    
        Setup Initial Razor Pages solution.
    
        Signed-off-by: Pablo Gonzalez <pablogr.gonzalez@gmail.com>

---
+++ src/BackgroundWorker/Program.cs
@@ -0,0 +1,10 @@
+using BackgroundWorker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
